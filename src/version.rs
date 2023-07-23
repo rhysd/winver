e99d81fca8f6f386835b93f8b5b818d96cec9add
rhysd <lin90162@yahoo.co.jp>
2023-07-24 01:03:21 +0900

    implement `Ord` and `Eq` for `WindowsVersion`

--- src/version.rs
+++ src/version.rs
@@ -1,6 +1,61 @@
-#[derive(Debug)]
+use std::cmp::Ordering;
+
+#[derive(Debug, PartialEq, Eq)]
 pub struct WindowsVersion {
     pub major: u32,
     pub minor: u32,
     pub build: u32,
+}
+
+impl WindowsVersion {
+    pub fn new(major: u32, minor: u32, build: u32) -> Self {
+        Self {
+            major,
+            minor,
+            build,
+        }
+    }
+}
+
+impl Ord for WindowsVersion {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.major
+            .cmp(&other.major)
+            .then_with(|| self.minor.cmp(&other.minor))
+            .then_with(|| self.build.cmp(&other.build))
+    }
+}
+
+impl PartialOrd for WindowsVersion {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let v = WindowsVersion::new(1, 2, 3);
+        assert_eq!(v.major, 1);
+        assert_eq!(v.minor, 2);
+        assert_eq!(v.build, 3);
+    }
+
+    #[test]
+    fn test_eq() {
+        assert_ne!(WindowsVersion::new(1, 2, 3), WindowsVersion::new(1, 2, 4));
+        assert_ne!(WindowsVersion::new(1, 2, 3), WindowsVersion::new(1, 3, 3));
+        assert_ne!(WindowsVersion::new(1, 2, 3), WindowsVersion::new(2, 2, 3));
+        assert_eq!(WindowsVersion::new(1, 2, 3), WindowsVersion::new(1, 2, 3));
+    }
+
+    #[test]
+    fn test_ord() {
+        assert!(WindowsVersion::new(1, 2, 3) < WindowsVersion::new(1, 2, 4));
+        assert!(WindowsVersion::new(1, 2, 3) < WindowsVersion::new(1, 3, 0));
+        assert!(WindowsVersion::new(0, 2, 3) < WindowsVersion::new(1, 0, 0));
+    }
 }
